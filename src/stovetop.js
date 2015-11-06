b9cd6329cf2451629f68533094c97fc8ac44ea6e
Svetoslav Nedkov <sveto.n@gmail.com>
2015-11-06 23:18:54 +0200

    Working video

--- src/stovetop.js
+++ src/stovetop.js
@@ -5,8 +5,27 @@ stovetop.config(function($interpolateProvider) {
     $interpolateProvider.endSymbol('}]}');
 });
 
-stovetop.controller('stovetop', ['$scope', function ($scope) {
+stovetop.controller('stovetop', ['$scope', '$uibModal', function ($scope, $modal) {
     $scope.click = function () {
         alert('Coming soon');
     };
+
+    $scope.showVideo = function (id) {
+        $modal.open({
+            animation: true,
+            controller: 'VideoController',
+            controllerAs: 'vm',
+            bindToController: true,
+            templateUrl: '/views/templates/video.html',
+            resolve: {
+                ytid: function () {
+                    return id;
+                }
+            },
+            size: 'lg'
+        });
+    };
+}])
+.controller('VideoController', ['$scope', 'ytid', '$sce', function ($scope, ytid, $sce) {
+    $scope.videoUrl = $sce.trustAsResourceUrl('https://www.youtube.com/embed/' + ytid);
 }]);
