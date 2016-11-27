a70cdc697ec496a9fc56132bdc8e97025b594288
Svetoslav Nedkov <sveto.n@gmail.com>
2016-11-27 14:03:23 +0200

    Sniff browser agent and serve alternative content if mobile

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var _ = require('lodash');
 var express = require('express');
-var hbs = require('hbs');
+var mobileBrowser = require('detect-mobile-browser');
 
 var fermentables = require('./data/fermentables.json');
 var hops = require('./data/hops.json');
@@ -9,8 +9,9 @@ var styles = require('./data/styles.json');
 
 var app = express();
 
-app.set('view engine', 'html');
-app.engine('html', hbs.__express);
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'jade');
+
 app.use('/src', express.static(__dirname + '/src'));
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/views/templates', express.static(__dirname + '/views/templates'));
@@ -18,8 +19,12 @@ app.use('/css', express.static(__dirname + '/css'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/trewbrews-calc-public', express.static(__dirname + '/trewbrews-calc-public'));
 
+app.use(mobileBrowser());
+
 app.get('/', function (req, res) {
-    res.render('main.hbs', {
+    var isMobile = req.SmartPhone.isAndroid() || req.SmartPhone.isIOS();
+    res.render('main.jade', {
+        mobile: !!isMobile
     });
 });
 
